9a38e268d268f823573384a01e3669a5abd7ddfd
Marvingarci <55658113+Marvingarci@users.noreply.github.com>
2021-12-14 16:17:37 -0600

    tabla

--- src/app/home/list-orders/list-orders.component.ts
+++ src/app/home/list-orders/list-orders.component.ts
@@ -11,18 +11,89 @@ export class ListOrdersComponent implements OnInit, OnChanges {
 
   constructor(private homeService:HomeService, private cookie:CookieService) { }
   @Input() event_id = null;
-  orders:any;
+  orders:any[] = [];
+  selectedAll:boolean = false;
+  titleselecteAll:String = "Select All";
+  selectedRowIds: Set<any> = new Set<any>();
+  count:number= 0;
+
+
   ngOnInit(): void {
    
   }
 
   ngOnChanges():void{
+    
     this.homeService.getSalesOrders({"event_id":this.event_id, "dbServer":this.cookie.get('dbServer')}).subscribe((data:any)=>{
       console.log(data)
       this.orders=data
     }, err =>{
       console.log(err)
     })
+  }
+
+  onAllRowClick(){
+    if(this.selectedAll == false){
+      for (var val of this.orders) {
+          this.selectedRowIds.add(val.yy_SOID_tx); 
+      }
+      this.selectedAll = true;
+      this.titleselecteAll = "Deselect";
+    }else{
+      for (var val of this.orders) {
+        this.selectedRowIds.delete(val.yy_SOID_tx); 
+      }
+      this.selectedAll = false;
+      this.titleselecteAll = "Select All";
+    }
+    this.countItemSelected();
+    
+  }
+
+  onRowClick(id: any) {
+    if(this.selectedRowIds.has(id)) {
+     this.selectedRowIds.delete(id);
+     this.titleselecteAll = "Select All";
+     this.selectedAll = false;
+    }
+    else {
+      this.selectedRowIds.add(id);
+      
+    var c = 0;
+    for (var val of this.orders) {
+      if(this.selectedRowIds.has(val.yy_SOID_tx)){
+        var c = c +1;
+        console.log("esta seleccionando")
+      }    
+    }
+    if(c == this.orders.length){
+        this.selectedAll = true;
+        this.titleselecteAll = "Deselect";
+      } 
+    }
+
+    this.countItemSelected();
+  } 
+  
+  countItemSelected(){
+    this.count = 0;
+    for (var val of this.orders) {
+      if(this.selectedRowIds.has(val.yy_SOID_tx)){
+        this.count = this.count +1;
+      }    
+    } 
+  }
+
+  rowIsSelected(id: any) {
+    return this.selectedRowIds.has(id);
+  }
+
+  getSelectedRows(){
+    return this.orders.filter(x => this.selectedRowIds.has(x.yy_SOID_tx));
+  }
+
+  onLogClick() {
+    console.log(this.getSelectedRows());
   }
 
 
--- src/app/home/wifi-settings/wifi-settings.component.ts
+++ src/app/home/wifi-settings/wifi-settings.component.ts
@@ -6,7 +6,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./wifi-settings.component.scss']
 })
 export class WifiSettingsComponent implements OnInit {
+  selectedRowIds: Set<number> = new Set<number>();
 
+  allRows: any[] = [
+    {id: 1, nom: 'A', prenom: 'X'},
+    {id: 2, nom: 'B', prenom: 'Y'},
+    {id: 3, nom: 'C', prenom: 'Z'},
+  ];
+
+  selectedId!: string;
+
+  onRowClick(id: number) {
+    if(this.selectedRowIds.has(id)) {
+     this.selectedRowIds.delete(id);
+    }
+    else {
+      this.selectedRowIds.add(id);
+    }
+  }
+
+  rowIsSelected(id: number) {
+    return this.selectedRowIds.has(id);
+  }
+
+  getSelectedRows(){
+    return this.allRows.filter(x => this.selectedRowIds.has(x.id));
+  }
+
+  onLogClick() {
+    console.log(this.getSelectedRows());
+  }
   constructor() { }
 
   ngOnInit(): void {
